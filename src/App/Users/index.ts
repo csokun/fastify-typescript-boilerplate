ac693d9c8a94273f0871969dabde7d6d23d96def
Sokun Chorn <chornsokun@gmail.com>
2021-04-17 11:40:39 +1000

    bundle fastify-opentelemetry

--- src/App/Users/index.ts
+++ src/App/Users/index.ts
@@ -1,10 +1,27 @@
 import { Application } from '@Shared/Types';
+import { FastifyRequest } from 'fastify';
 import * as Schemas from './schemas';
 
 export async function Users(app: Application): Promise<void> {
-    // @dev - perform app health-check
-    app.get('/users', { schema: Schemas.usersSchema }, async () => [
+    const userList = [
         { id: 1, name: 'Joe Smith' },
         { id: 2, name: 'Carl Franston' }
-    ]);
+    ];
+
+    app.get('/users', { schema: Schemas.usersSchema }, async () => userList);
+
+    app.get('/users/telemetry', { schema: Schemas.usersSchema }, async (request: FastifyRequest) => {
+        const {
+            activeSpan,
+            tracer,
+            // context,
+            // extract,
+            // inject,
+        } = request.openTelemetry()
+        // Spans started in a wrapped route will automatically be children of the activeSpan.
+        const childSpan = tracer.startSpan(`${activeSpan.context.name} - child process`)
+        // doSomeWork()
+        childSpan.end()
+        return userList;
+    });
 }
